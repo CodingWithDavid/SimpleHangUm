acfb17ecf74c1d611313e635c872f180429af4ec
David Gallivan <33528347+CodingWithDavid@users.noreply.github.com>
2020-11-09 16:07:43 -0700

    Initial project load

---
+++ HangUm/Services/WordService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangUm.Services
+{
+    public class WordService
+    {
+
+        private List<string> WordsToUse { get; set; } 
+
+        public WordService()
+        {
+            WordsToUse = new List<string>();
+
+            //load data file
+            WordsToUse.AddRange(LoadTextFile());
+        }
+
+        private List<string> LoadTextFile()
+        {
+            List<string> result = new List<string>();
+            //make sure the file exists
+            if (File.Exists("wwwroot/data/words.dat"))
+            {
+                try
+                {
+                    string data = File.ReadAllText("wwwroot/data/words.dat");
+                    string[] sdata = data.Replace('\n', ' ').Trim().Split('\r');
+                    result.AddRange(sdata.Where(str => !string.IsNullOrEmpty(str)));
+                }
+                catch
+                {
+
+                }
+            }
+            return result;
+        }
+
+
+        public async Task<string> Get()
+        {
+            string result = "";
+
+            if (WordsToUse.Any())
+            {
+                Random r = new Random();
+                int index = r.Next(1, WordsToUse.Count - 1);
+                try
+                {
+                    result = WordsToUse.ElementAt(index).Trim();
+                }
+                catch
+                {
+                    result = WordsToUse.ElementAt(0).Trim();
+                }
+            }
+            await Task.CompletedTask;
+            return result;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+# Hang Um for Blazor
+
+This is a simple hang man game written in Blazor.  The objective of the project was not to focus on the UI but to be able to code the interactions of the game 
+
+by [David Gallivan](http://twitter.com/CodingwithDavid)
+
+
+
+## Getting Started
+
+1. Clone this repository
+
+   ```Command Line
+   git clone https://github.com/CodingWithDavid/SimpleHangUm
+   cd SimpleHangUm
+   ```
+
+1.	Open in Visual Studio or Visual Code
+a.	With Visual Code you will need to install the C# extensions
+2.	Press F5
+
+## What's in the App
+
+1. Simple UI that show the standard hang man frame
+2. Keyboard support for entering in guesses
+3. Displaying wrong guesses
+4. Notification on a letter in twice
+5. Notification on game end
+6. Notification on game won
+
+
+
+## Problems or Suggestions
+
+[Open an issue here]( https://github.com/CodingWithDavid/SimpleHangUm/issues)
+
+## Thank You
+
+
+## Resources
+
+- [VS Code](https://code.visualstudio.com)
+- [Visual Studio]( https://visualstudio.microsoft.com/)
+
+
+
